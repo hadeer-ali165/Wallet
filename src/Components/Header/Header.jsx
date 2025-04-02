997ceea12795a48b253a0f453459de56b05de2e8
Hadeer <hadeeraly165@gmail.com>
2025-04-02 18:27:11 +0200

    Update images in header file

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,14 @@
 import head from './Header.module.css';
+import imgs1 from '../../../public/ImagesHead/1.jfif';
+import imgs2 from '../../../public/ImagesHead/2.jfif';
+import imgs3 from '../../../public/ImagesHead/3.jfif';
+import imgs4 from '../../../public/ImagesHead/4.jfif';
+import imgs5 from '../../../public/ImagesHead/5.png';
+import imgs6 from '../../../public/ImagesHead/6.png';
+import imgs7 from '../../../public/ImagesHead/7.jfif';
+import imgs8 from '../../../public/ImagesHead/8.jfif';
+import imgs9 from '../../../public/ImagesHead/9.png';
+import imgs10 from '../../../public/ImagesHead/10.jfif';
 import ImagesHeader from '../../Data/Data';
 const Header = () => {
     // let[imgs , setImgs] = useState(ImagesHeader)
@@ -21,38 +31,38 @@ return (
                 <div>
                 <div className={`${head.lineImage} flex`}>
                     <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/1.jfif" alt="" />
+                        <img src={imgs1} alt="" />
                     </div>
                     <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/2.jfif" alt="" />
+                        <img src={imgs2} alt="" />
                     </div>
                     <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/3.jfif" alt="" />
-                    </div>
-                </div>
-                <div className={`${head.lineImage} flex`}>
-                    <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/4.jfif" alt="" />
-                    </div>
-                    <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/5.png" alt="" />
-                    </div>
-                    <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/6.png" alt="" />
+                        <img src={imgs3} alt="" />
                     </div>
                 </div>
                 <div className={`${head.lineImage} flex`}>
                     <div className={head.img1}>
-                        <img className={`w-[450px]`} src="../../../public/ImagesHead/7.jfif" alt="" />
+                        <img src={imgs4} alt="" />
                     </div>
                     <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/8.jfif" alt="" />
+                        <img src={imgs5} alt="" />
                     </div>
                     <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/9.png" alt="" />
+                        <img src={imgs6} alt="" />
+                    </div>
+                </div>
+                <div className={`${head.lineImage} flex`}>
+                    <div className={head.img1}>
+                        <img className={`w-[450px]`} src={imgs7} alt="" />
                     </div>
                     <div className={head.img1}>
-                        <img src="../../../public/ImagesHead/10.jfif" alt="" />
+                        <img src={imgs8} alt="" />
+                    </div>
+                    <div className={head.img1}>
+                        <img src={imgs9} alt="" />
+                    </div>
+                    <div className={head.img1}>
+                        <img src={imgs10} alt="" />
                     </div>
                 </div>
                 </div>
